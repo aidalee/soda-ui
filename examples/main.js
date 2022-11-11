93869f4c78547ab2771bec0d01002703e3351942
please <please@xiaotata.local>
2022-11-11 17:57:21 +0800

    fix

--- examples/main.js
+++ examples/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import './styles/index.scss'
 import DemoBlock from './layout/demo-block.vue'
 import SodaUI from '@soda-f2e/ui'
+console.log('SodaUI', SodaUI)
 import '@soda-f2e/ui/es/style.css'
 const app = createApp(App)
 app.component(DemoBlock.name, DemoBlock)
